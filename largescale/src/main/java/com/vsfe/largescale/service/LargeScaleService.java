82442cd46a2fcf12569403760cf766f41dc30f7a
VSFe <klm03025@gmail.com>
2024-10-09 00:43:28 +0900

    임시 커밋

--- largescale/src/main/java/com/vsfe/largescale/service/LargeScaleService.java
+++ largescale/src/main/java/com/vsfe/largescale/service/LargeScaleService.java
@@ -5,6 +5,7 @@ import com.vsfe.largescale.repository.AccountRepository;
 import com.vsfe.largescale.repository.TransactionRepository;
 import com.vsfe.largescale.repository.UserRepository;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,18 +19,20 @@ public class LargeScaleService {
     private final TransactionRepository transactionRepository;
     private final UserRepository userRepository;
 
+    public final int MAX_SIZE = 100;
+
     /**
      * 최신 유저의 목록을 가져온다.
      * 최신의 조건이 어떻게 될까?
      * @param count
      * @return
      */
-    public List<User> getUserInfo(@Positive @Max(100) int count) {
+    public List<User> getUserInfo(@Positive @Max(MAX_SIZE) int count) {
 //        return userRepository.findRecentUpdatedUsers(count);
         return userRepository.findRecentCreatedUsers(count);
     }
 
-    public void getTransactions() {
+    public void getTransactions(@NotEmpty String userId, @NotEmpty String lastTransactionId, @Positive @Max(MAX_SIZE) int count) {
 
     }
 
---
+++ largescale/src/main/java/com/vsfe/largescale/util/C4QueryExecuteUtil.java
@@ -0,0 +1,65 @@
+package com.vsfe.largescale.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.IntSupplier;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class C4QueryExecuteUtil {
+    /**
+     * limit 보다 affected 가 작을 때 까지 반복 수행한다.
+     * delete/update 쿼리를 limit 로 반복 수행할 때 사용한다.
+     * @param limit
+     * @param executeSupplier
+     * @return
+     */
+    public static int execute(int limit, IntSupplier executeSupplier) {
+        var totalAffected = 0;
+        var affected = 0;
+
+        do {
+            affected = executeSupplier.getAsInt();
+            totalAffected += affected;
+        } while (affected >= limit);
+
+        return totalAffected;
+    }
+
+    /**
+     * select 의 조회 결과가 더 나오지 않을 때 까지 반복하여 update/delete 연산을 수핸한다.
+     * 주의: consumer가 update 인 경우, selectFunction 에서 조회된 값이 consumer 동작 이후 재 조회되지 않아야 한다.
+     * @param limit
+     * @param selectFunction
+     * @param resultConsumer
+     * @param <T>
+     */
+    public static <T> void selectAndExecute(int limit, IntFunction<List<T>> selectFunction, Consumer<List<T>> resultConsumer) {
+        List<T> resultList;
+        while (CollectionUtils.isNotEmpty(resultList = selectFunction.apply(limit))) {
+            resultConsumer.accept(resultList);
+        }
+    }
+
+    /**
+     * Cursor Paging 을 적용하여 select 를 수행한 후, 조회된 결과로 비즈니스 로직을 수행한다.
+     * @param limit
+     * @param selectFunction
+     * @param resultConsumer
+     * @param <T>
+     */
+    public static <T> void selectAndExecuteWithCursor(long limit, Function<T, List<T>> selectFunction, Consumer<List<T>> resultConsumer) {
+        List<T> resultList = null;
+        do {
+            resultList = selectFunction.apply(resultList != null ? resultList.get(resultList.size() - 1) : null);
+            if (!resultList.isEmpty()) {
+                resultConsumer.accept(resultList);
+            }
+        } while (resultList.size() >= limit);
+    }
+}
